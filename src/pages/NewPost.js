eb5860575ff8ab2b313c1bdddb9be01672d9fc78
tobiaswollendorfer <t.wollendorfer@gmail.com>
2022-09-20 15:59:06 +0200

    second commit

---
+++ src/pages/NewPost.js
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Input from "../components/Input";
+import Bloglist from "../components/Bloglist";
+
+function NewPost() {
+  const [blogPosts, setBlogPosts] = useState([]);
+
+  function deletepost(ID) {
+    setBlogPosts((prevBlogPosts) => {
+      const updatedBlogPosts = prevBlogPosts.filter((blog) => blog.id !== ID);
+      return updatedBlogPosts;
+    });
+  }
+  //need to have value and label for checkbox??
+  function addBlog(value, label, id, username, title, date, post) {
+    setBlogPosts((prevBlogPosts) => {
+      const updatedBlogPosts = [...prevBlogPosts];
+
+      updatedBlogPosts.unshift({
+        value: value,
+        label: label,
+        id: id,
+        username: username,
+        date: date,
+        title: title,
+        post: post,
+      });
+      return updatedBlogPosts;
+    });
+  }
+
+  console.log(blogPosts);
+  return (
+    <div>
+      <Input onAddBlogPost={addBlog} />
+      <Bloglist bloglist={blogPosts} deletepost={deletepost} />
+    </div>
+  );
+}
+
+export default NewPost;
